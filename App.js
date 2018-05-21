dafb330c5f6b301f1a52c620cc2fba049e60a2a4
Lily Yang <zq.yang.lily@gmail.com>
2018-05-20 17:58:42 -0700

    status bar hidden, head showing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default class App extends React.Component {
   constructor(){
@@ -72,9 +72,14 @@ export default class App extends React.Component {
   // }
   render() {
     console.log(this.state);
+    let price = this.state.price/100;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar hidden={true} />
+        <View style={styles.header}>
+          <Text style={styles.name}>{this.state.name}</Text>
+          <Text style={styles.price}>{price}</Text>
+        </View>
       </View>
     );
   }
@@ -85,6 +90,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
+  header:{
+    backgroundColor:'pink',
+    width:'100%',
+    height:45,
+    paddingTop:10,
+  },
+  name:{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  price:{
+    position:'absolute',
+    right:0,
+    marginRight:20,
+    paddingTop:13
+  }
 });
