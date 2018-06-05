12a2c3601375e074a5445b2348f37bddaf79328b
Lily Yang <zq.yang.lily@gmail.com>
2018-06-05 13:41:40 -0700

    choice logic changed

--- App.js
+++ App.js
@@ -28,11 +28,11 @@ export default class App extends React.Component {
       options.forEach((option)=>{
         let { min, chosen} = option;
         let itemName = option.name;
-        //add to alert if not meeting requiring min 
+        //add to alert if not meeting requiring min
         if(min && chosen.length < min){
           alert += `Please at least choose ${min} ${itemName} \n`;
         }else{
-          items = chosen.join(" ");
+          let items = chosen.join(" ");
           order += `${itemName}: ${items} \n`;
         }
       });
@@ -54,11 +54,11 @@ export default class App extends React.Component {
             {text: 'Confirm', onPress: () => console.log('OK Pressed')},
             {text: 'Modify', onPress: () => console.log('Modify Pressed')},
           ]
-        )
+        );
       }
-    }
+    };
   }
-  
+
   _handleQuantity(operation,title,name,addPrice,max){
     return ()=>{
       let {options} = this.state;
@@ -69,23 +69,68 @@ export default class App extends React.Component {
       let prevPrice = this.state.price;
       //when operation is add, handle array/delta change
       if(operation === "add"){
-        chosenArr.push(name);
         //delete overflow chosen item, when no maxium quantity restrict(max = 0),will not trigger delete
-        if(max && chosenArr.length > max){
-          removedChoice = chosenArr.shift();
-          let subChoiceB = find(currentOption.items,{name:removedChoice});
-          subChoiceB.quantity = subChoiceB.quantity - 1;
-          prevPrice = prevPrice - subChoiceB.price;
+        if(max && chosenArr.length >= max){
+          for(let i = 0; i < chosenArr.length; i++){
+            let a = chosenArr[i];
+            if(chosenArr[i]===name){
+              continue;
+            }else{
+              let removedChoice = chosenArr.splice(i,1)[0];
+              //reduce removed choice quantity
+              let subChoiceB = find(currentOption.items,{name:removedChoice});
+              subChoiceB.quantity = subChoiceB.quantity - 1;
+              //adjust price
+              prevPrice = prevPrice - subChoiceB.price;
+              chosenArr.push(name);
+              //find clicked subitem in options
+              let subChoiceA = find(currentOption.items,{name:name});
+              //add quantity to this item
+              subChoiceA.quantity = subChoiceA.quantity + 1;
+              //update options with changed item
+              options[index] = currentOption;
+              //adjust price
+              let totalPrice = prevPrice + addPrice;
+              //change state with the new info
+              this.setState({price:totalPrice,options:options});
+              return;
+            }
+          }
+          Alert.alert(
+            'Hi Dear',
+            'No',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ]
+          );
+          return;
+        }else{
+          chosenArr.push(name);
+          //find clicked subitem in options
+          let subChoiceA = find(currentOption.items,{name:name});
+          //add quantity to this item
+          subChoiceA.quantity = subChoiceA.quantity + 1;
+          //update options with changed item
+          options[index] = currentOption;
+          //adjust price
+          let totalPrice = prevPrice + addPrice;
+          //change state with the new info
+          this.setState({price:totalPrice,options:options});
         }
-        delta = 1;
       }else{
+        //remove clicked subitem from chosenArr
         remove(chosenArr,(n)=> n === name);
+        //find clicked subitem in options
+        let subChoiceA = find(currentOption.items,{name:name});
+        //minus quantity to this item
+        subChoiceA.quantity = subChoiceA.quantity - 1;
+        //update options with changed item
+        options[index] = currentOption;
+        //adjust price
+        let totalPrice = prevPrice + (addPrice * -1);
+        //change state with the new info
+        this.setState({price:totalPrice,options:options});
       }
-      let subChoiceA = find(currentOption.items,{name:name});
-      subChoiceA.quantity = subChoiceA.quantity + delta;
-      options[index] = currentOption;
-      let totalPrice = prevPrice + (addPrice * delta);
-      this.setState({price:totalPrice,options:options});
     };
   }
   _renderItem(){
@@ -124,11 +169,11 @@ export default class App extends React.Component {
           <Text>+{showPrice}</Text>
           </View>);
         }
-    }
+    };
   }
   render() {
     if(this.state.name){
-      
+
       let price = this.state.price/100;
       //pretreat options for sectionList
       let choices = this.state.options.map((option)=>{
@@ -136,23 +181,23 @@ export default class App extends React.Component {
       });
       return (
         <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <View style={styles.header}>
-        <Text style={styles.name}>{this.state.name}</Text>
-        <TouchableOpacity 
-        style={styles.price}
-        onPress={this._handleSubmitCheck()}>
-        <Text>{price}</Text>
-        </TouchableOpacity>
-        </View>
-        <View style={styles.sectionList}>
-        <SectionList
-        sections={choices}
-        renderItem={this._renderItem()}
-        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>Choose {section.title}</Text>}
-        keyExtractor={(item, index) => index}
-        />
-        </View>
+          <StatusBar hidden={true} />
+          <View style={styles.header}>
+            <Text style={styles.name}>{this.state.name}</Text>
+            <TouchableOpacity
+            style={styles.price}
+            onPress={this._handleSubmitCheck()}>
+              <Text>{price}</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.sectionList}>
+            <SectionList
+            sections={choices}
+            renderItem={this._renderItem()}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>Choose {section.title}</Text>}
+            keyExtractor={(item, index) => index}
+            />
+          </View>
         </View>
       );
     }else{
